5eb6cba7426ff2e60ffb031153564429365c186a
onyeka mmakwe <notmaks@yahoo.com>
2020-04-20 02:56:00 +0100

    loading spinners for single country

--- src/components/pages/SinglePage/SinglePage.jsx
+++ src/components/pages/SinglePage/SinglePage.jsx
@@ -4,6 +4,7 @@ import back from '../../../assets/back.svg';
 import Button from '../../atoms/Button/Button';
 import Header from '../../atoms/Header/Header';
 import { Link } from 'react-router-dom';
+import Skeleton from 'react-loading-skeleton';
 
 const SinglePage = ({ match, history }) => {
   const [countryDetail, setCountryDetail] = useState(null);
@@ -36,62 +37,80 @@ const SinglePage = ({ match, history }) => {
         </Button>
         <div className='country'>
           <div>
-            <img src={countryDetail?.flag} alt={countryCode.name} />
+            {isLoading ? (
+              <Skeleton height={400} />
+            ) : (
+              <img src={countryDetail?.flag} alt={countryCode.name} />
+            )}
           </div>
           <div>
-            <Header headerSize='lg'>{countryDetail?.name}</Header>
+            <Header headerSize='lg'>
+              {isLoading ? <Skeleton width={300} /> : countryDetail?.name}
+            </Header>
             <div className='country__detail'>
-              <div>
-                <div className='country__text'>
-                  <Header headerSize='sm'>Native Name</Header>
-                  {`: ${countryDetail?.nativeName}`}
-                </div>
+              {isLoading ? (
+                <Skeleton width={600} height={300} />
+              ) : (
+                <>
+                  <div>
+                    <div className='country__text'>
+                      <Header headerSize='sm'>Native Name</Header>
+                      {`: ${countryDetail?.nativeName}`}
+                    </div>
 
-                <div className='country__text'>
-                  <Header headerSize='sm'>Population</Header>
-                  {`: ${countryDetail?.population}`}
-                </div>
-                <div className='country__text'>
-                  <Header headerSize='sm'>Region</Header>
-                  {`: ${countryDetail?.region}`}
-                </div>
-                <div className='country__text'>
-                  <Header headerSize='sm'>Capital</Header>
-                  {`: ${countryDetail?.capital}`}
-                </div>
-              </div>
-              <div>
-                <div className='country__text'>
-                  <Header headerSize='sm'>Numeric Code</Header>
-                  {`: ${countryDetail?.numericCode}`}
-                </div>
-                <div className='country__text'>
-                  <Header headerSize='sm'>Currencies</Header>
-                  {countryDetail?.currencies?.map(
-                    (currency) => `: ${currency.name} `,
-                  )}
-                </div>
-                <div className='country__text'>
-                  <Header headerSize='sm'>Languages</Header>
-                  {countryDetail?.languages?.map(
-                    (language) => `: ${language.name} `,
-                  )}
-                </div>
-              </div>
+                    <div className='country__text'>
+                      <Header headerSize='sm'>Population</Header>
+                      {`: ${countryDetail?.population}`}
+                    </div>
+                    <div className='country__text'>
+                      <Header headerSize='sm'>Region</Header>
+                      {`: ${countryDetail?.region}`}
+                    </div>
+                    <div className='country__text'>
+                      <Header headerSize='sm'>Capital</Header>
+                      {`: ${countryDetail?.capital}`}
+                    </div>
+                  </div>
+                  <div>
+                    <div className='country__text'>
+                      <Header headerSize='sm'>Numeric Code</Header>
+                      {`: ${countryDetail?.numericCode}`}
+                    </div>
+                    <div className='country__text'>
+                      <Header headerSize='sm'>Currencies</Header>
+                      {countryDetail?.currencies?.map(
+                        (currency) => `: ${currency.name} `,
+                      )}
+                    </div>
+                    <div className='country__text'>
+                      <Header headerSize='sm'>Languages</Header>
+                      {countryDetail?.languages?.map(
+                        (language) => `: ${language.name} `,
+                      )}
+                    </div>
+                  </div>
+                </>
+              )}
             </div>
             <div className='country__extra'>
-              <Header headerSize='sm'>Border Countries</Header>:
-              <ul className='country__buttons'>
-                {countryDetail?.borders?.length === 0
-                  ? 'None'
-                  : countryDetail?.borders?.map((border) => (
-                      <li key={border}>
-                        <Link to={`/${border.toLowerCase()}`}>
-                          <Button btnStyle='btn--primary'>{border}</Button>
-                        </Link>
-                      </li>
-                    ))}
-              </ul>
+              {isLoading ? (
+                <Skeleton width={500} height={150} />
+              ) : (
+                <>
+                  <Header headerSize='sm'>Border Countries</Header>:
+                  <ul className='country__buttons'>
+                    {countryDetail?.borders?.length === 0
+                      ? 'None'
+                      : countryDetail?.borders?.map((border) => (
+                          <li key={border}>
+                            <Link to={`/${border.toLowerCase()}`}>
+                              <Button btnStyle='btn--primary'>{border}</Button>
+                            </Link>
+                          </li>
+                        ))}
+                  </ul>
+                </>
+              )}
             </div>
           </div>
         </div>
